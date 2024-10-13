f423c85ddf347da92fd903898057cd7d3d6b3a87
Tom Laumen <tom.laumen@boskalis.com>
2024-10-13 23:42:38 +0200

    more robust db interface

--- pydalio/constants.py
+++ pydalio/constants.py
@@ -1,3 +1,8 @@
+# Environment variables
 YAML_PATH_ENV_VARIABLE_KEY: str = "YAML_PATH"
 DB_PATH_ENV_VARIABLE_KEY: str = "DB_PATH"
+
+# Database related constants
 DB_NAME = "dalio.db"
+MAX_LEN_CASE_DESCR = 100
+PRINCIPLES_TABLE_NAME = "principles"
