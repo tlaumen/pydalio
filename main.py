d8123c4efadd485fb3362396df8c90f9175a66e2
Tom Laumen <tom.laumen@boskalis.com>
2024-10-13 19:59:58 +0200

    removed print

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ from pydalio.constants import DB_NAME, YAML_PATH_ENV_VARIABLE_KEY
 from pydalio.principle import yaml_loader
 from pydalio.db import create_db, initiliaze_tables
 
-print(YAML_PATH_ENV_VARIABLE_KEY)
 db_path = Path(__file__).parent / DB_NAME
 # try:
 create_db(Path(__file__).parent)
